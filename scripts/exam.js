9c4f52ffce2a914aa0fa04faa13131bacc8caa5e
xiaowei <119865@shfang.net>
2018-06-01 00:15:51 +0800

    use api

--- scripts/exam.js
+++ scripts/exam.js
@@ -1,8 +1,13 @@
 ﻿let counter = 0; //定时运行次数 == 消耗时间秒数
 let submitStatus = "未交卷"; //未交卷, 交卷中, 已交卷, 交卷失败
 let answer = [];
+const ep_id = getQueryString('ep_id')
+const user_id = getQueryString('user_id')
+let er_id;
+let eq1_ids;
+let eq2_ids;
 $(function () {
-    console.log('helloWord exam')
+    console.log('ep_id is ' + ep_id + ', user_id is ' + user_id)
     initExamList();
 })
 
@@ -12,113 +17,12 @@ function initExamList() {
 
 function getExamData() {
     //TODO
-    $.get('https://dangjain.ishoubei.com:8443/exam/plan/2', function(data){
+    $.get('https://dangjain.ishoubei.com:8443/exam/test?ep_id=' + ep_id + '&user_id=' + user_id, function(data){
         console.log(data);
-        const exam = {
-            examId: 1,
-            title: '中国共产党纪律处分条例测试',
-            totalScore: 100,
-            duration: 90,  //分钟
-            status: "考试中", //未开考, 考试中, 已考完
-            remainingSecond: 1 * 60 - 32, //剩余秒数 -- status === '考试中' 才有意义
-            uScore: "85", //考生得分 --- status === '已考完' 才有意义
-            question: [
-                {
-                    index: 1,
-                    indexTxt: '一',
-                    key: '选择题',
-                    qList: [
-                        {
-                            id: 1,
-                            showIndex: '1',
-                            title: '下列___项是最根本的党内法规，是管党治党的总规矩:',
-                            uAnswer: 'D',
-                            items: [
-                                {
-                                    key: 'A',
-                                    value: '《中国共产党纪律处分条例》'
-                                },
-                                {
-                                    key: 'B',
-                                    value: '《中国共产党章程》'
-                                },
-                                {
-                                    key: 'C',
-                                    value: '《中国共产党廉洁自律准则》'
-                                },
-                                {
-                                    key: 'D',
-                                    value: '《中国共产党党员领导干部廉洁从政若干准则》'
-                                }
-                            ]
-                        },
-                        {
-                            id: 2,
-                            showIndex: '2',
-                            title: '以下_____项不是对党员的纪律处分:',
-                            // uAnswer: 'C',
-                            items: [
-                                {
-                                    key: 'A',
-                                    value: '记过'
-                                },
-                                {
-                                    key: 'B',
-                                    value: '警告'
-                                },
-                                {
-                                    key: 'C',
-                                    value: '撤销党内职务'
-                                },
-                                {
-                                    key: 'D',
-                                    value: '开除党籍'
-                                }
-                            ]
-                        }
-                    ]
-                },
-                {
-                    index: 2,
-                    indexTxt: '二',
-                    key: '判断题',
-                    qList: [
-                        {
-                            id: 3,
-                            showIndex: '1',
-                            title: '下列___项是最根本的党内法规，是管党治党的总规矩:',
-                            uAnswer: 'B',
-                            items: [
-                                {
-                                    key: 'A',
-                                    value: '对'
-                                },
-                                {
-                                    key: 'B',
-                                    value: '错'
-                                },
-                            ]
-                        },
-                        {
-                            id: 4,
-                            showIndex: '2',
-                            title: '以下_____项不是对党员的纪律处分:',
-                            uAnswer: 'A',
-                            items: [
-                                {
-                                    key: 'A',
-                                    value: '对'
-                                },
-                                {
-                                    key: 'B',
-                                    value: '错'
-                                },
-                            ]
-                        },
-                    ]
-                }
-            ]
-        };
+        er_id = data.er_id;
+        eq1_ids = data.eq1_ids ;
+        eq2_ids = data.eq2_ids ;
+        const exam = convertAPIDataToExam(data)
         //set selected
         exam.question = exam.question.map(function(item){
             item.qList.map(function(qItem){
@@ -128,6 +32,7 @@ function getExamData() {
                     }
                     return x;
                 })
+                answer.push({id: qItem.id, userAnswerKey: qItem.uAnswer ? qItem.uAnswer : ''})
                 return qItem;
             })
             return item
@@ -136,12 +41,169 @@ function getExamData() {
         initListBody(exam);
         initListFooter(exam);
         if(exam.status !== '已考完'){
-            const rTime  = exam.remainingSecond ? Number(exam.remainingSecond) : exam.duration * 60;
+            const rTime  = Number(exam.remainingSecond);
             startCountDown(rTime);
             startLoopUserAnswer(rTime)
         }
     });
 };
+
+function convertAPIDataToExam(data) {
+    const exam = {
+        examId: data.er_id,
+        title: data.plan.ep_name,
+        status: getStatus(data.er_state),
+        remainingSecond: getRemainingSecond(data.plan.epe_time), 
+        uScore: data.er_mark,
+        question: getQuestions(data)
+    };
+    exam.totalScore = getTotalScore(exam.question);
+    console.log(exam);
+    return exam;
+}
+
+function getTotalScore(data){ 
+    const allScore = data.map(function(item){
+        return item.typeScore
+    }).reduce(function(a,b){
+        return a + b
+    })
+    return allScore;
+}
+
+function getStatus(status){ 
+    return status === 5 ? "已考完" : "考试中" //考试中, 已考完
+}
+
+function getRemainingSecond(endTime){ 
+    const endTimeStamp = new Date(endTime).getTime()
+    const currentTimeStamp = new Date().getTime()
+    return parseInt((endTimeStamp - currentTimeStamp) / 1000); //剩余秒数 -- status === '考试中' 才有意义
+}
+
+function getQuestions(data){
+    const questions = [];
+    const sInfo = {
+        indexTxt: '一',
+        key: '选择题',
+        averageScore: data.plan.eq2_val, //每题分数
+        typeScore: data.plan.eq2_val * data.plan.eq2_cnt,//类型总分
+        qList: getDetailQList(data.eq2_obj, data.eq2_res, data.eq2_ids, '选择题')
+    };//选择题
+    questions.push(sInfo)
+    const jInfo = {
+        indexTxt: '二',
+        key: '判断题',
+        averageScore: data.plan.eq1_val, //每题分数
+        typeScore: data.plan.eq1_val * data.plan.eq1_cnt,//类型总分
+        qList: getDetailQList(data.eq1_obj, data.eq1_res, data.eq1_ids, '判断题')
+    };//判断题
+    questions.push(jInfo)
+    return questions;
+}
+
+
+function getDetailQList(data, res, sortIds, type){
+    const rIdList = sortIds.split(","); //保证顺序一致
+    console.log(res)
+    const rResList = res.split(","); //保证顺序一致
+    const qList = [];
+    data.map(function(q){
+        const item =  {
+            id: q.eq_id,
+            showIndex: getShowIndex(q.eq_id, rIdList),
+            title: q.question,
+            uAnswer: getUAnswer(q.eq_id, rIdList, rResList), //用户的答案
+            answer: q.answer, //题目的 answer
+            items: getSelectItems(q, type)
+        };
+        item.isRight = item.uAnswer === item.answer //是否答题正确
+        qList.push(item);
+        return q;
+    })
+    return qList.sort(function(a,b) { //以显示顺序排序
+        return a.showIndex - b.showIndex
+    });
+}
+
+function getSelectItems(data, type){
+    let items = []
+    if(type === '判断题'){
+        items = [
+            {
+                key: 'V',
+                value: ''
+            },
+            {
+                key: 'X',
+                value: ''
+            }
+        ]
+    }
+    else { //选择题
+        items = [
+        {
+            key: 'A',
+            value: data.op_a
+        },
+        {
+            key: 'B',
+            value: data.op_b
+        },
+        {
+            key: 'C',
+            value: data.op_c
+        },
+        {
+            key: 'D',
+            value: data.op_d
+        }]
+        
+    }
+   return items
+}
+
+function getShowIndex(id, rIdList){
+    let returnId;
+    for(let i=0; i<rIdList.length; i++){
+        if(Number(rIdList[i]) === id) {
+            returnId = i + 1;
+        }
+    }
+    console.log('----')
+    console.log(returnId)
+    return returnId;
+}
+
+function getUAnswer(id, rIdList, rResList){
+    console.log(rResList)
+    const list = [];
+    for(let i=0; i<rIdList.length; i++){
+        list.push({id: rIdList[i], res:rResList[i]})
+    }
+    console.log(list)
+    console.log(list.find(function(item){
+        return item.id === id;
+    }))
+    return list.find(function(item){
+        return Number(item.id) === id;
+    }).res
+}
+
+function getLastEqRes(answer, ids){
+    const realAnswerList = [];
+    ids.split(',').map(function(id){
+        const userA = answer.find(function(item){
+            return Number(item.id) === Number(id);
+        }).userAnswerKey
+        console.log(userA)
+        realAnswerList.push(userA)
+        return id
+    })
+    return realAnswerList.reduce(function(a,b){
+         return a + ',' + b
+    })
+}
 
 function initListHeader(exam) {
     var list_header_template = [
@@ -150,7 +212,7 @@ function initListHeader(exam) {
         '<a href="javascript:void(0);" class="weui-media-box weui-media-box_appmsg">',
         '<div class="weui-media-box__bd">',
         '<h4 class="weui-media-box__title">{{title}}</h4>',
-        '<p class="weui-media-box__desc">（总分：{{totalScore}}分；测试时间：{{duration}}分钟）</p>',
+        '<p class="weui-media-box__desc">（总分：{{totalScore}}分）</p>',
         '</div>',
         '</a>',
         '</div>',
@@ -164,7 +226,7 @@ function initListHeader(exam) {
 function initListBody(exam) {
     const list_body_template = [
         '{{#data}}<div class="weui-panel weui-panel_access test">',
-        '<div class="weui-panel__hd">{{indexTxt}}、{{key}}</div>',
+        '<div class="weui-panel__hd">{{indexTxt}}、{{key}} (每题{{averageScore}}分, 总共{{typeScore}}分)</div>',
         '<div class="weui-panel__bd">',
         '{{#qList}}<span href="javascript:void(0);" class="weui-media-box weui-media-box_appmsg">',
         '<div class="weui-media-box__bd">',
@@ -199,7 +261,7 @@ function initListFooter(exam) {
         submitStatus = "已交卷"
     }
     else {
-        const time  = exam.remainingSecond ? Number(exam.remainingSecond) : exam.duration * 60;
+        const time  = Number(exam.remainingSecond) ;
         const timeTxt = getTimeText(time) 
         text = timeTxt + "&nbsp; &nbsp; 交&nbsp;卷";
     }
@@ -252,32 +314,29 @@ function finishExam(){
     if(submitStatus === "未交卷" || submitStatus === "交卷失败"){
         console.log('-----用户的答案是')
         console.log(answer)
-        // TODO
+        const eq1_res = getLastEqRes(answer, eq1_ids)
+        const eq2_res = getLastEqRes(answer, eq2_ids)
+        const postData = {
+            er_id,
+            eq1_res,
+            eq2_res,
+            er_state: 4
+        }
+        console.log(postData)
         submitStatus = "交卷中"
         $("#btn-submit").html("交卷中"); 
         console.log(submitStatus)
         $.ajax({  
             type: "post",  
-            url: "https://dangjain.ishoubei.com:8443/exam/plan",  
+            url: "https://dangjain.ishoubei.com:8443/exam/test",  
             contentType : "application/x-www-form-urlencoded; charset=UTF-8",  
-            data: $.param({
-                user_id: "xuehaifeng",
-                ep_name: "六盘水钟山区学习习近平智慧党建考试",
-                ep_type: "一试一卷",
-                eps_time: "2018-06-01 08:00:00",
-                epe_time: "2018-06-01 16:00:00",
-                ep_limit: 120,
-                eq1_cnt: 10,
-                eq1_val: 6,
-                eq2_cnt: 10,
-                eq2_val: 5
-            }),  
+            data: $.param(postData),  
             success: function (res) {  
                 console.log(res)
                 submitStatus = "已交卷"
                 $("#btn-submit").html(submitStatus); 
                 console.log(submitStatus)
-                window.location = "test.html"; //TODO: 写成云学习中心的 url
+                window.location = 'test.html?ep_id=' + ep_id + '&user_id=' + user_id;
             },
             error: function () {
                 /*错误信息处理*/
@@ -297,36 +356,32 @@ function loopUserAnswer(){
      console.log('轮训用户答题情况')
      console.log('-----用户的答案是')
      console.log(answer)
-    // TODO
-    $.ajax({  
+     const eq1_res = getLastEqRes(answer, eq1_ids)
+     const eq2_res = getLastEqRes(answer, eq2_ids)
+     const postData = {
+         er_id,
+         eq1_res,
+         eq2_res
+     }
+     $.ajax({  
         type: "post",  
-        url: "https://dangjain.ishoubei.com:8443/exam/plan",  
+        url: "https://dangjain.ishoubei.com:8443/exam/test",  
         contentType : "application/x-www-form-urlencoded; charset=UTF-8",  
-        data: $.param({
-            user_id: "xuehaifeng",
-            ep_name: "六盘水钟山区学习习近平智慧党建考试",
-            ep_type: "一试一卷",
-            eps_time: "2018-06-01 08:00:00",
-            epe_time: "2018-06-01 16:00:00",
-            ep_limit: 120,
-            eq1_cnt: 10,
-            eq1_val: 6,
-            eq2_cnt: 10,
-            eq2_val: 5
-        }),  
+        data: $.param(postData),  
         success: function (res) {  
             console.log(res)
-            // window.location = "testRel.html";    
-        }  
+        } 
     }); 
 }
 
 function getTimeText(time){
-    const minute = parseInt(Number(time) / 60) 
+    const hour = parseInt(Number(time) / 3600) 
+    const minute = parseInt((Number(time) -  hour * 3600)/ 60) 
     const second = parseInt(Number(time) % 60) 
+    const hourTxt = hour < 10 ? '0' + hour : '' + hour
     const minuteTxt = minute < 10 ? '0' + minute : '' + minute
     const secondTxt = second < 10 ? '0' + second : '' + second
-    return  minuteTxt + ' : ' + secondTxt
+    return  hourTxt + ' : ' + minuteTxt + ' : ' + secondTxt
 };
 
 function setUserAnswer(id, key) {
@@ -337,6 +392,7 @@ function setUserAnswer(id, key) {
         return item.id !== id
     })
     answer.push({id, userAnswerKey: key})
+    console.log(answer)
 };
 
 function sendUserAnswer(data) {
@@ -364,3 +420,9 @@ function sendUserAnswer(data) {
         }  
     });  
 };
+
+function getQueryString(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(r[2]); return null;
+}
