7da5397f3b625d5cccee6b4a2dc1c7cd3c2ced4a
xiaowei <119865@shfang.net>
2018-07-12 02:44:46 +0800

    bug fix: 考试中心 - 考试记录功能（没有关联真实数据）and 考试中心 - 开始考试, 继续考试, 重新考试功能

--- scripts/examForResult.js
+++ scripts/examForResult.js
@@ -1,9 +1,9 @@
 ﻿let counter = 0; //定时运行次数 == 消耗时间秒数
 let submitStatus = "未交卷"; //未交卷, 交卷中, 已交卷, 交卷失败
 let answer = [];
-const ep_id = getQueryString('ep_id')
+const er_id = getQueryString('er_id')
 const user_id = getQueryString('user_id')
-let er_id;
+let ep_id;
 let eq1_ids;
 let eq2_ids;
 
@@ -21,7 +21,7 @@ function initConst(info) {
 
 $(function () {
     //getUserInfo(url, initConst); //TODO
-    console.log('ep_id is ' + ep_id + ', user_id is ' + user_id)
+    console.log('er_id is ' + er_id + ', user_id is ' + user_id)
     initExamList();
 })
 
@@ -31,11 +31,11 @@ function initExamList() {
 
 function getExamData() {
     //TODO
-    $.post('https://dangjain.ishoubei.com/exam/result?ep_id=' + ep_id + '&user_id=' + user_id, function(data){
+    $.get('https://dangjain.ishoubei.com/exam/test?er_id=' + er_id, function(data){
         console.log(data);
-        er_id = data.er_id;
         eq1_ids = data.eq1_ids ;
         eq2_ids = data.eq2_ids ;
+        ep_id = data.plan.ep_id;
         const exam = convertAPIDataToExam(data)
         //set selected
         exam.question = exam.question.map(function(item){
@@ -54,17 +54,11 @@ function getExamData() {
         if(!exam.uScore && exam.remainingSecond <= 0){ //0分 --> 未答 或 未提交
             console.log('已过答题时间 将 status 设置为已考完')
             exam.status = '已考完'
-            window.location = 'examForResult.html?er_id=' + er_id + '&user_id=' + user_id;
         }
         console.log(exam.status)
         initListHeader(exam);
-        initListBody(exam);
+        initListBodyForExamed(exam);
         initListFooter(exam);
-        if(exam.status !== '已考完'){
-            const rTime  = Number(exam.remainingSecond); //TODO: 剩余时间
-            startCountDown(rTime);
-            // startLoopUserAnswer(rTime)  //TODO: 时间轮训
-        }
     });
 };
 
@@ -246,18 +240,21 @@ function initListHeader(exam) {
     $("#list_header").html(Mustache.render(list_header_template, exam))
 };
 
-function initListBody(exam) {
+function initListBodyForExamed(exam) {
     const list_body_template = [
         '{{#data}}<div class="weui-panel weui-panel_access test">',
         '<div class="weui-panel__hd">{{indexTxt}}、{{key}} (每题{{averageScore}}分, 总共{{typeScore}}分)</div>',
         '<div class="weui-panel__bd">',
         '{{#qList}}<span href="javascript:void(0);" class="weui-media-box weui-media-box_appmsg">',
         '<div class="weui-media-box__bd">',
+        '{{^isRight}}<img class="rel_img" src="../img/wrong.png"/>{{/isRight}}',
+        '{{#isRight}}<img class="rel_img" src="../img/right.png"/>{{/isRight}}',
         '<p class="weui-media-box__desc">{{showIndex}}. {{title}}</p>',
         '<div class="weui-cells weui-cells_radio">',
         '{{#items}}<label class="weui-cell weui-check__label" for="radio{{id}}{{key}}">',
         '<div class="weui-cell__bd">',
-        '<div class="item_div">{{key}}. {{value}} </div>',
+        '{{#isRightAnswer}}<div class="item_answer_div">{{key}}. {{value}} </div>{{/isRightAnswer}}',
+        '{{^isRightAnswer}}<div class="item_div">{{key}}. {{value}} </div>{{/isRightAnswer}}',
         '</div>',
         '<div class="weui-cell__ft">',
         '{{^selected}}<input type="radio" class="weui-check" name="radio{{id}}" id="radio{{id}}{{key}}" value="{{key}}" onclick="setUserAnswer({{id}}, this.value)">{{/selected}}',
@@ -275,174 +272,40 @@ function initListBody(exam) {
     Mustache.parse(list_body_template);
     const data = exam.question;
     $("#list_body").html(Mustache.render(list_body_template, { data: data }))
-};
+}
 
 function initListFooter(exam) {
     let text = '';
-    if(exam.status === '已考完'){
+    // if(exam.status === '已考完'){
         text = "得分: " + exam.uScore + "分" +  "&nbsp; &nbsp; &nbsp 点击重考";
         submitStatus = "已交卷"
-    }
-    else {
-        const time  = Number(exam.remainingSecond) ;
-        const timeTxt = getTimeText(time) 
-        text = timeTxt + "&nbsp; &nbsp; 交&nbsp;卷";
-    }
+    // }
+    // else {
+    //     const time  = Number(exam.remainingSecond) ;
+    //     const timeTxt = getTimeText(time) 
+    //     text = timeTxt + "&nbsp; &nbsp; 交&nbsp;卷";
+    // }
     console.log(text)
     $("#btn-submit").html(text);
 };
 
-function startCountDown(remainingTime){
-    var timer = function () {
-        setTimeout(function () {
-            console.log('-------------startCountDown')
-            if(submitStatus === "未交卷") {
-                counter++;
-                const rTime = remainingTime - counter;
-                console.log(rTime);
-                if(rTime <= 0){
-                    $("#btn-submit").html(getTimeText(rTime) + "交卷中"); 
-                    finishExam()
-                }
-                else{
-                    $("#btn-submit").html(getTimeText(rTime) + "&nbsp; &nbsp; 交&nbsp;卷");
-                    timer();
-                }
-            }
-        }, 1000);
-
-    }
-    timer();
-}
-
-
-// function startLoopUserAnswer(remainingTime){
-//     var timer = function () {
-//         setTimeout(function () {
-//             console.log('-------------loopAnswer')
-//             if(submitStatus === "未交卷") {
-//                 const rTime = remainingTime - counter;
-//                 console.log(rTime);
-//                 if(rTime > 0){
-//                     loopUserAnswer()
-//                     timer();
-//                 }
-//             }
-//         }, 10000);
-//     }
-//     timer();
-// }
 function finishExam(){
-    //交卷
-    if(submitStatus === "未交卷" || submitStatus === "交卷失败"){
-        console.log('-----用户的答案是')
-        console.log(answer)
-        const eq1_res = getLastEqRes(answer, eq1_ids)
-        const eq2_res = getLastEqRes(answer, eq2_ids)
-        const postData = {
-            er_id,
-            eq1_res,
-            eq2_res,
-            er_state: 4
-        }
-        console.log(postData)
-        submitStatus = "交卷中"
-        $("#btn-submit").html("交卷中"); 
-        console.log(submitStatus)
-        $.ajax({  
-            type: "post",  
-            url: "https://dangjain.ishoubei.com/exam/test",  
-            contentType : "application/x-www-form-urlencoded; charset=UTF-8",  
-            data: $.param(postData),  
-            success: function (res) {  
-                console.log(res)
-                submitStatus = "已交卷"
-                $("#btn-submit").html(submitStatus); 
-                console.log(submitStatus)
-                window.location = 'examForResult.html?er_id=' + er_id + '&user_id=' + user_id;
-            },
-            error: function () {
-                /*错误信息处理*/
-                submitStatus = "交卷失败"
-                $("#btn-submit").html("交卷失败, 点击重新交卷"); 
-                console.log(submitStatus)
-            }  
-        });  
-    }
-    else{
-        console.log(submitStatus)
-    }
+    //重考
+    console.log(submitStatus)
+    console.log('重新考试')
+    window.location = 'test.html?ep_id=' + ep_id + '&user_id=' + user_id;
 }
-
-function loopUserAnswer(){
-     //轮训用户答题情况
-     console.log('轮训用户答题情况')
-     console.log('-----用户的答案是')
-     console.log(answer)
-     const eq1_res = getLastEqRes(answer, eq1_ids)
-     const eq2_res = getLastEqRes(answer, eq2_ids)
-     const postData = {
-         er_id,
-         eq1_res,
-         eq2_res
-     }
-     $.ajax({  
-        type: "post",  
-        url: "https://dangjain.ishoubei.com/exam/test",  
-        contentType : "application/x-www-form-urlencoded; charset=UTF-8",  
-        data: $.param(postData),  
-        success: function (res) {  
-            console.log(res)
-        } 
-    }); 
-}
-
-function getTimeText(time){
-    const hour = parseInt(Number(time) / 3600) 
-    const minute = parseInt((Number(time) -  hour * 3600)/ 60) 
-    const second = parseInt(Number(time) % 60) 
-    const hourTxt = hour < 10 ? '0' + hour : '' + hour
-    const minuteTxt = minute < 10 ? '0' + minute : '' + minute
-    const secondTxt = second < 10 ? '0' + second : '' + second
-    return  hourTxt + ' : ' + minuteTxt + ' : ' + secondTxt
-};
 
 function setUserAnswer(id, key) {
     console.log('---------')
     console.log(id);
     console.log(key);
-    answer = answer.filter(function(item){
-        return item.id !== id
-    })
-    answer.push({id, userAnswerKey: key})
-    console.log(answer)
-    loopUserAnswer()
-};
-
-function sendUserAnswer(data) {
-    console.log(data)
-    console.log('---sendUserAnswer')
-    $.ajax({  
-        type: "post",  
-        url: "https://dangjain.ishoubei.com/exam/plan",  
-        contentType : "application/x-www-form-urlencoded; charset=UTF-8",  
-        data: $.param({
-            user_id: "xuehaifeng",
-            ep_name: "六盘水钟山区学习习近平智慧党建考试",
-            ep_type: "一试一卷",
-            eps_time: "2018-06-01 08:00:00",
-            epe_time: "2018-06-01 16:00:00",
-            ep_limit: 120,
-            eq1_cnt: 10,
-            eq1_val: 6,
-            eq2_cnt: 10,
-            eq2_val: 5
-        }),  
-        success: function (res) {  
-            console.log(res)
-            // window.location = "testRel.html";    
-        }  
-    });  
+    // answer = answer.filter(function(item){
+    //     return item.id !== id
+    // })
+    // answer.push({id, userAnswerKey: key})
+    // console.log(answer)
+    // loopUserAnswer()
 };
 
 function getQueryString(name) {
--- scripts/myExamHistoryList.js
+++ scripts/myExamHistoryList.js
@@ -1,7 +1,6 @@
 let user_id = 'xuehaifeng'
 var userInfo = {};
 var url = location.href;
-//  alert(location.href);
 function initConst(info) {
     userInfo = info;
     user_id = userInfo.emplId
@@ -24,11 +23,22 @@ function getExamList() {
     $.get('https://dangjain.ishoubei.com/exam/result?user_id=' + user_id, function(data){
         console.log(data);
         data = data.rows.map(function(examPlan){
-            examPlan.ep_name = examPlan.ep_name === undefined ? '考试测试01' : examPlan.ep_name
+            console.log(examPlan.plan)
+            const isDeletePlan = examPlan.plan === undefined || examPlan.plan === null
+            if(!isDeletePlan){
+                examPlan.isDeleteOrOverTimePlan = newDate(examPlan.plan.epe_time).getTime() -  Date.now() < 0
+            }
+            else {
+                examPlan.isDeleteOrOverTimePlan = isDeletePlan
+            }
+            const name = examPlan.plan === undefined || examPlan.plan === null ? '已删除 plan考试标题' : examPlan.plan.ep_name
+            console.log(name)
+            examPlan.ep_name = name
+            examPlan.ep_id = examPlan.plan === undefined || examPlan.plan === null ? -1 : examPlan.plan.ep_id
             examPlan.isInExaming = examPlan.er_state === 2
             examPlan.isExamed = !examPlan.isInExaming
             examPlan.show_status = examPlan.isInExaming ? '考试中' : '已考完'
-            examPlan.exam_btn_txt = examPlan.show_status === '考试中' ? '继续考试' : '查看考试结果' 
+            examPlan.exam_btn_txt = examPlan.show_status === '考试中' ? '继续考试' : '重新考试' 
             return examPlan
         })
         console.log(data);
@@ -47,11 +57,11 @@ function initListBody(data) {
                     '</div>{{/isInExaming}}',
                     '{{#isExamed}}<div class="item btn">',       
                     '<span>考试得分：{{er_mark}}</span>',                
-                    '<a href="test.html?ep_id={{ep_id}}&user_id={{user_id}}&restart=true">重新考试</a>',                   
+                    '{{^isDeleteOrOverTimePlan}}<a href="examForResult.html?er_id={{er_id}}&user_id={{user_id}}&restart=true">查看考试结果</a>{{/isDeleteOrOverTimePlan}}',                   
                     '</div>{{/isExamed}}',
                     '<div class="item btn">',       
                         '<span>状态: {{show_status}}</span>',                   
-                        '<a href="test.html?ep_id={{ep_id}}&user_id={{user_id}}">{{exam_btn_txt}}</a>',                   
+                        '{{^isDeleteOrOverTimePlan}}<a href="test.html?ep_id={{ep_id}}&user_id={{user_id}}">{{exam_btn_txt}}</a>{{/isDeleteOrOverTimePlan}}',                   
                     '</div>',
                 '</div>',
             '</div>',
@@ -68,9 +78,9 @@ function getQueryString(name) {
     if (r != null) return unescape(r[2]); return null;
 }
 
-// function newDate(strdate) {  
-//     var arr = strdate.split(/[- : \/]/);  
-//     console.log(arr);
-//     date = new Date(arr[0], arr[1]-1, arr[2], arr[3], arr[4], arr[5]);  
-//     return date;  
-// } 
+function newDate(strdate) {  
+    var arr = strdate.split(/[- : \/]/);  
+    console.log(arr);
+    date = new Date(arr[0], arr[1]-1, arr[2], arr[3], arr[4], arr[5]);  
+    return date;  
+} 
